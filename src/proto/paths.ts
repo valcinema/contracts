00dbd6005c3523cede06f8a697b3dc68daa47174
Valentyn <val.bakhmatskyi@gmail.com>
2026-05-21 16:56:20 +0300

    feat: add account service proto

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -1,5 +1,6 @@
 import { join } from 'path';
 
 export const PROTO_PATH = {
-    AUTH: join(__dirname, '../../proto/auth.proto')
+    AUTH: join(__dirname, '../../proto/auth.proto'),
+    ACCOUNT: join(__dirname, '../../proto/account.proto')
 } as const;
