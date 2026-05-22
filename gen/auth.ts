0fa78404c1ed9f876f54ccf6891955a9935c2475
Valentyn <val.bakhmatskyi@gmail.com>
2026-05-22 12:50:08 +0300

    feat: add email/phone change flow to account

--- gen/auth.ts
+++ gen/auth.ts
@@ -19,19 +19,47 @@ export interface SendOtpResponse {
   ok: boolean;
 }
 
+export interface VerifyOtpRequest {
+  identifier: string;
+  type: string;
+  code: string;
+}
+
+export interface VerifyOtpResponse {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface RefreshRequest {
+  refreshToken: string;
+}
+
+export interface RefreshResponse {
+  accessToken: string;
+  refreshToken: string;
+}
+
 export const AUTH_V1_PACKAGE_NAME = "auth.v1";
 
 export interface AuthServiceClient {
   sendOtp(request: SendOtpRequest): Observable<SendOtpResponse>;
+
+  verifyOtp(request: VerifyOtpRequest): Observable<VerifyOtpResponse>;
+
+  refresh(request: RefreshRequest): Observable<RefreshResponse>;
 }
 
 export interface AuthServiceController {
   sendOtp(request: SendOtpRequest): Promise<SendOtpResponse> | Observable<SendOtpResponse> | SendOtpResponse;
+
+  verifyOtp(request: VerifyOtpRequest): Promise<VerifyOtpResponse> | Observable<VerifyOtpResponse> | VerifyOtpResponse;
+
+  refresh(request: RefreshRequest): Promise<RefreshResponse> | Observable<RefreshResponse> | RefreshResponse;
 }
 
 export function AuthServiceControllerMethods() {
   return function (constructor: Function) {
-    const grpcMethods: string[] = ["sendOtp"];
+    const grpcMethods: string[] = ["sendOtp", "verifyOtp", "refresh"];
     for (const method of grpcMethods) {
       const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
       GrpcMethod("AuthService", method)(constructor.prototype[method], method, descriptor);
